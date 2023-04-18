ec1099e60acc7f231faac36fc99c3705b40598f1
Wissam Mouhaidli <wmhaidly@gmail.com>
2023-04-18 10:56:38 -0300

    Second commit Main

--- Test.java
+++ Test.java
@@ -1,38 +1,8 @@
 //Version_2 Wissam 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
-
-
-        System.out.println("branch-3");
-        System.out.println("branch-3");
-
-        System.out.println("branch-4");
-        System.out.println("branch-6.1");
-
-
-        System.out.println("branch-Wissam-2");
-
-        System.out.println("branch-7.1");
-
-        System.out.println("branch-7.2");
-        System.out.println("branch-Wissam-2");
-        System.out.println("branch-Kee-1.0");
-
-
-        System.out.println("branch-Wissam-3");
-        System.out.println("main branch update");
-
-        System.out.println("version4");
-        System.out.println("version4");
-        System.out.println("version4");
-        System.out.println("https://learngitbranching.js.org");
-        System.out.println("main");
-
-        System.out.println("trash");
-        System.out.println("trash again");
-        System.out.println("again trash");
+        System.out.println("Starting from Main");
+       
 
 
     }
