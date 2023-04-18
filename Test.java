c1bcac580a867adc5148f9984aa2f9daa1208d81
Wissam Mouhaidli <wmhaidly@gmail.com>
2023-04-18 11:12:30 -0300

    CD on Main 2 continues

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Starting from Main");
         System.out.println("CD on Main");
+         System.out.println("CD on Main2");
        
 
 
