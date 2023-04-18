3bff95407584e388019848f365a1596601c54d6f
Wissam Mouhaidli <wmhaidly@gmail.com>
2023-04-18 11:15:41 -0300

    WMTestBranch update

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Starting from Main");
         System.out.println("Branched out - WMTestBranch");
+        System.out.println("Branched out - WMTestBranch update");
        
 
 
