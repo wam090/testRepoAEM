0ae8e8c8b29cde1d1e5b55ffc72885aa4b561fe5
Wissam Mouhaidli <wmhaidly@gmail.com>
2023-08-02 08:51:47 -0300

    Update 1 | Cleansing

--- Test.java
+++ Test.java
@@ -7,15 +7,9 @@ public class Test {
          System.out.println("CD on Main2");
 
     
-        System.out.println("Branched out - WMTestBranch update");
-        System.out.println("Branched out - DevBranch");
-        System.out.println("change on Dev_ update_1");
-        System.out.println("change on Dev_ tested_on _Dev");
-        System.out.println("Development continues");
-        System.out.println("Development continues_2");
-        System.out.println("Development continues_3");
-        System.out.println("Development continues_4");
-        System.out.println("Change from LD to Origin");
+        System.out.println("Cleansing - Update 1 on Local");
+        System.out.println("Cleansing - Update 2 on Local to push to origin");
+      
        
 
 
