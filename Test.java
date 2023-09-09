24a4ddf1d3ef98c141f1f80d26650cebf56b1a7f
Wissam Mhaidly <wmhaidly@gmail.com>
2023-09-09 14:31:06 -0300

    Update 9

--- Test.java
+++ Test.java
@@ -1,22 +1,9 @@
 //Version_2 Wissam 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Starting from Main");
-
-        System.out.println("CD on Main");
-         System.out.println("CD on Main2");
-
-    
-        System.out.println("Cleansing - Update 1 on Local");
-        System.out.println("Cleansing - Update 2 on Local to push to origin");
-        System.out.println("Cleansing - Update 3 on Local to push to origin");
-        System.out.println("Cleansing - Update 4 on Local to push to origin");
-        System.out.println("Cleansing - Update 5 on Local to push to origin");
-        System.out.println("Cleansing - Update 6 on Local to push to origin");
-        System.out.println("Cleansing - Update 6.5 on Local to push to origin");
-        System.out.println("Cleansing - Update 7 on Local to push to origin");
-        System.out.println("Cleansing - Update 7.5 on Local to push to origin");
-        System.out.println("Cleansing - Update 8 on Local to push to origin");
+        
+        System.out.println("Cleansing - Update 9 on Local - push to Origin");
+        
       
        
 
